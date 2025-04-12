90b58fdf31428347338511cd592b335944f38c56
Stanislav Kolesnikov <123827132+Gh0StCK@users.noreply.github.com>
2025-04-12 11:36:25 +0200

    Add files via upload

--- addon/__init__.py
+++ addon/__init__.py
@@ -1,34 +1,26 @@
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTIBILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-# General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <https://www.gnu.org/licenses/>.
+from . import utils
+from . import props
+from . import icons
+from . import ops
+from . import ui
+from . import timers
 
 
-bl_info = {
-    'name': 'SIMPLE TABS',
-    'author': 'bonjorno7, Chipp Walters, MasterXeon1001, Updated for Blender 4.0+ by gh0stck',
-    'description': 'Organize the Blender sidebar (Community Update)',
-    'blender': (4, 0, 0),
-    'version': (2, 0, 0),
-    'location': 'View3D',
-    'category': '3D View',
-}
-
-
-from . import addon
+modules = (
+    utils,
+    props,
+    icons,
+    ops,
+    ui,
+    timers,
+)
 
 
 def register():
-    addon.register()
+    for module in modules:
+        module.register()
 
 
 def unregister():
-    addon.unregister()
+    for module in reversed(modules):
+        module.unregister()
